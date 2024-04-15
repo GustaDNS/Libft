e9a53db9443912e8771418080d88bd6efe0ab380
Gustavo <88571393+GustaDNS@users.noreply.github.com>
2024-04-15 17:30:30 +0100

    Add files via upload
    add some functions and fix some bugs

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gudaniel <gudaniel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/15 16:40:44 by gudaniel          #+#    #+#             */
+/*   Updated: 2024/04/15 17:27:44 by gudaniel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*list;
+
+	list = (t_list *)malloc(sizeof(t_list));
+	if (!list)
+		return (NULL);
+}
